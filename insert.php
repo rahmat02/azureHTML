cd13fc7fc6f5d6feb08194587af842d5b46999bd
rahmat02 <35823567+rahmat02@users.noreply.github.com>
2019-03-22 21:26:30 +0800

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,29 @@
+<?php
+//include "koneksi.php";
+$conn = mysqli_connect('rahmatmysqlserver.mysql.database.azure.com','rahmat@rahmatmysqlserver','Rmt025524123','dbdicoding');
+
+if ($conn){
+$name = $_POST['name'];
+$email = $_POST['email'];
+$job = $_POST['job'];
+$psw = $_POST['psw'];
+
+$sql = "INSERT INTO tb_register(name, email,job,psw) VALUES('$name', '$email','$job','$psw')";
+
+if(mysqli_query($conn,$sql))
+{
+	echo "Registerd Successfully";
+
+}
+else
+{
+	echo mysqli_error($conn);
+}
+}
+else
+{
+echo "error";
+}
+?>
+
+
